9a367b09e712279c39ec42c28d002f76829e6e7f
mohanthatha <mohanchowdary79@gmail.com>
2022-02-11 14:34:31 +0530

    firstcommit

---
+++ src/main/java/net/codejava/DemoController.java
@@ -0,0 +1,24 @@
+package net.codejava;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class DemoController {
+
+	@GetMapping("/name")
+	public String getName() {
+		return "Play Java!";
+}
+
+	@GetMapping("/address")
+	public String getAddress() {
+		return "India!";
+}
+	@GetMapping("/status")
+	public String getStatus() {
+		return "Active!";
+}
+}
